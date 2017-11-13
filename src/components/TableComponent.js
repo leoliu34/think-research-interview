27706d488eb31cf0c711ed27cf298af78ccec8c1
Leo Liu <j336liu@gmail.com>
2017-11-13 18:27:53 -0500

    Update link to the specific pages

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -1,11 +1,11 @@
 import { Icon, Label, Menu, Table, Button, Dropdown, Modal, Header } from 'semantic-ui-react'
 import React, { Component } from 'react'
-
-const options = [
-  { key: 'edit', icon: 'edit', text: 'Edit', value: 'edit' },
-  { key: 'delete', icon: 'delete', text: 'Destroy', value: 'delete' },
-  { key: 'show', icon: 'unhide', text: 'Show', value: 'show' },
-]
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import EditPatientView from './EditPatientView'
 
 export default class TableComponent extends Component {
 	constructor(props) {
@@ -15,13 +15,14 @@ export default class TableComponent extends Component {
 			body: props.body,
 			removePatient: props.removePatient,
 			confirmDelete: false,
-			deletePatientMRN: 0
+			currentPatientMRN: 0
 		}
 		this.renderBody = this.renderBody.bind(this)
 		this.performAction = this.performAction.bind(this)
 		this.removePatient = this.removePatient.bind(this)
 		this.modalOpen = this.modalOpen.bind(this)
 		this.modalClose = this.modalClose.bind(this)
+		this.onClickDelete = this.onClickDelete.bind(this)
 	}
 
 	renderHeader(key) {
@@ -44,36 +45,29 @@ export default class TableComponent extends Component {
 					})
 				}
 				<Table.Cell>
-					<Dropdown ref={patient.mrn} additionLabel={patient.mrn} text="Actions" options={options} onChange={this.performAction} floating button />
-					{
-						console.log(this.refs)
-					}
+					<Dropdown ref={patient.mrn} additionLabel={patient.mrn} text="Actions" onClick={this.performAction} floating button >
+						<Dropdown.Menu>
+							<Dropdown.Item key="show" icon="unhide" text={<Link style={{ color: '#000000' }} to={{ pathname: '/patients/' + this.state.currentPatientMRN }}>Show</Link>} value="show"/>
+							<Dropdown.Item key="edit" icon="edit" text={<Link style={{ color: '#000000' }} to={{ pathname: '/edit/' + this.state.currentPatientMRN }}>Edit</Link>} value="show"/>
+							<Dropdown.Item key="delete" icon="delete" text="Destroy" value="delete" onClick={this.onClickDelete} />
+						</Dropdown.Menu>
+					</Dropdown>
 				</Table.Cell>
 			</Table.Row>
 		)
 	}
 
-	performAction(event, data) {
-		console.log(event)
-		console.log(event.DOMEventTarget)
-		console.log(data)
-		if (data.value === 'edit') {
-			this.refs[data.additionLabel].text = 'edit'
+	onClickDelete(event, data) {
+		this.modalOpen()
+	}
 
-		}
-		else if (data.value === 'show') {
-			this.refs[data.additionLabel].text = 'show'
-		}
-		else if (data.value === 'delete') {
-			this.refs[data.additionLabel].text = 'delete'
-			let patientMRN = parseInt(data.additionLabel)
-			this.setState({ deletePatientMRN: patientMRN })
-			this.modalOpen()
-		}
+	performAction(event, data) {
+		let patientMRN = parseInt(data.additionLabel)
+		this.setState({ currentPatientMRN: patientMRN })
 	}
 
 	removePatient() {
-		this.state.removePatient(this.state.deletePatientMRN)
+		this.state.removePatient(this.state.currentPatientMRN)
 		this.modalClose()
 	}
 
@@ -87,67 +81,67 @@ export default class TableComponent extends Component {
 
 	render() {
 		return (
-			<div>
-			<Modal
-				open={this.state.confirmDelete}
-				onClose={this.modalClose}
-				basic
-			    size='small'
-		    >
-		        <Header icon='browser' content='Confirm Delete' />
-		        <Modal.Content>
-		          <h3>Are you sure you want to delete the current patient's entry?</h3>
-		        </Modal.Content>
-		        <Modal.Actions>
-		          <Button color='red' onClick={this.modalClose} inverted>
-		            No
-		          </Button>
-		          <Button color='green' onClick={this.removePatient} inverted>
-		            <Icon name='checkmark' /> Yes
-		          </Button>
-		        </Modal.Actions>
-		      </Modal>
-			<Table celled striped>
-			    <Table.Header>
-			      <Table.Row>
-			        <Table.HeaderCell colSpan={Object.keys(this.state.header).length-1}>{this.props.title}</Table.HeaderCell>
-			        <Table.HeaderCell><Button primary>Add User</Button></Table.HeaderCell>
-			      </Table.Row>
-			      <Table.Row>
-			      {
-			      	Object
-					.keys(this.state.header)
-					.map(key =>  this.renderHeader(key))
-			      }
-			      </Table.Row>
-			    </Table.Header>
-			    <Table.Body>
-			    {
-			    	Object
-			    	.keys(this.state.body)
-			    	.map(key => this.renderBody(key))
-			    }
-			    </Table.Body>
-			    <Table.Footer>
-			      <Table.Row>
-			        <Table.HeaderCell colSpan='3'>
-			          <Menu floated='right' pagination>
-			            <Menu.Item as='a' icon>
-			              <Icon name='left chevron' />
-			            </Menu.Item>
-			            <Menu.Item as='a'>1</Menu.Item>
-			            <Menu.Item as='a'>2</Menu.Item>
-			            <Menu.Item as='a'>3</Menu.Item>
-			            <Menu.Item as='a'>4</Menu.Item>
-			            <Menu.Item as='a' icon>
-			              <Icon name='right chevron' />
-			            </Menu.Item>
-			          </Menu>
-			        </Table.HeaderCell>
-			      </Table.Row>
-			    </Table.Footer>
-			</Table>
-			</div>
+				<div>
+				<Modal
+					open={this.state.confirmDelete}
+					onClose={this.modalClose}
+					basic
+				    size='small'
+			    >
+			        <Header icon='browser' content='Confirm Delete' />
+			        <Modal.Content>
+			          <h3>Are you sure you want to delete the current patient's entry?</h3>
+			        </Modal.Content>
+			        <Modal.Actions>
+			          <Button color='red' onClick={this.modalClose} inverted>
+			            No
+			          </Button>
+			          <Button color='green' onClick={this.removePatient} inverted>
+			            <Icon name='checkmark' /> Yes
+			          </Button>
+			        </Modal.Actions>
+			      </Modal>
+				<Table celled striped>
+				    <Table.Header>
+				      <Table.Row>
+				        <Table.HeaderCell colSpan={Object.keys(this.state.header).length-1}>{this.props.title}</Table.HeaderCell>
+				        <Table.HeaderCell><Button primary><Link style={{color: '#FFF'}}to="/add/patient">Add User</Link></Button></Table.HeaderCell>
+				      </Table.Row>
+				      <Table.Row>
+				      {
+				      	Object
+						.keys(this.state.header)
+						.map(key =>  this.renderHeader(key))
+				      }
+				      </Table.Row>
+				    </Table.Header>
+				    <Table.Body>
+				    {
+				    	Object
+				    	.keys(this.state.body)
+				    	.map(key => this.renderBody(key))
+				    }
+				    </Table.Body>
+				    <Table.Footer>
+				      <Table.Row>
+				        <Table.HeaderCell colSpan='3'>
+				          <Menu floated='right' pagination>
+				            <Menu.Item as='a' icon>
+				              <Icon name='left chevron' />
+				            </Menu.Item>
+				            <Menu.Item as='a'>1</Menu.Item>
+				            <Menu.Item as='a'>2</Menu.Item>
+				            <Menu.Item as='a'>3</Menu.Item>
+				            <Menu.Item as='a'>4</Menu.Item>
+				            <Menu.Item as='a' icon>
+				              <Icon name='right chevron' />
+				            </Menu.Item>
+				          </Menu>
+				        </Table.HeaderCell>
+				      </Table.Row>
+				    </Table.Footer>
+				</Table>
+				</div>
 		)
 	}
 }
