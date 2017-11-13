223146c950b512f1a9d451523b23e3478c43c019
Leo Liu <j336liu@gmail.com>
2017-11-13 18:26:57 -0500

    Add React Router and setup routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import PatientOverview from './components/PatientOverview'
+import EditPatientView from './components/EditPatientView'
+import PatientDetailsView from './components/PatientDetailsView'
+import CreatePatientView from './components/CreatePatientView'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  BrowserHistory,
+  Switch
+} from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PatientOverview />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={PatientOverview} />
+          <Route exact path="/edit/:mrn" component={EditPatientView} />
+          <Route exact path="/patients/:mrn" component={PatientDetailsView} />
+          <Route exact path="/add/patient" component={CreatePatientView} />
+        </Switch>
+      </Router>
     );
   }
 }
