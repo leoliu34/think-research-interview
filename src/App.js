e8046026cd378167d3a7ebd5a7472f2cba641d7e
Leo Liu <j336liu@gmail.com>
2017-11-13 17:20:20 -0500

    Setup using create-react-app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PatientOverview from './components/PatientOverview'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PatientOverview />
+      </div>
+    );
+  }
+}
+
+export default App;
