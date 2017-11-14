3076302a14d65515dc3fe4682cd671c697f1841d
Leo Liu <j336liu@gmail.com>
2017-11-14 03:52:47 -0500

    Add Routing and paths for all components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import PatientOverview from './components/PatientOverview'
 import EditPatientView from './components/EditPatientView'
 import PatientDetailsView from './components/PatientDetailsView'
 import CreatePatientView from './components/CreatePatientView'
+import CreateEncounterView from './components/CreateEncounterView'
+import EncounterDetailsView from './components/EncounterDetailsView'
+import EditEncounterView from './components/EditEncounterView'
+import exportPatient from './utils/loadData'
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  BrowserHistory,
   Switch
 } from 'react-router-dom'
 
@@ -22,49 +24,56 @@ class App extends Component {
         col3: 'Last Name',
         col4: 'Actions'
       },
-      patients: {
-        patient_1001: {
-          mrn: '1001',
-          firstName: 'Leo',
-          lastName: 'Liu',
-          middleName: 'L',
-          weight: '135',
-          height: '178',
-        },
-        patient_1002: {
-          mrn: '1002',
-          firstName: 'Lee',
-          lastName: 'Liu',
-          middleName: 'L',
-          weight: '135',
-          height: '178',
-        }
+      encountersHeader: {
+        col1: 'Visit Number',
+        col2: 'Admitted At',
+        col3: 'Discharged At',
+        col4: 'Actions'
       },
+      patients: exportPatient(),
       removePatient: this.removePatient.bind(this),
+      removeEncounter: this.removeEncounter.bind(this)
     }
     this.addPatient = this.addPatient.bind(this)
+    this.addEncounter = this.addEncounter.bind(this)
+    this.editEncounter = this.editEncounter.bind(this)
     this.editPatient = this.editPatient.bind(this)
+    this.getPatient = this.getPatient.bind(this)
+    this.getEncounter = this.getEncounter.bind(this)
   }
 
   removePatient(mrn) {
     let newState = {...this.state}
     let deletedKey = null
     Object.keys(newState.patients).map((key, index) => {
-      console.log(newState.patients[key].mrn)
       if (newState.patients[key].mrn === mrn) {
         deletedKey = key
       }
+      return null
     })
     delete newState.patients[deletedKey]
     this.setState({...newState})
   }
 
+  removeEncounter(mrn, deleteKey) {
+    let newState = {...this.state}
+    delete newState.patients['patient_' + mrn].encounters[deleteKey]
+    this.setState({...newState})
+  }
+
   addPatient(patient) {
-    console.log(patient)
     let newPatients = {...this.state.patients}
     newPatients['patient_' + patient.mrn] = patient
     this.setState({
       patients: newPatients
+    })
+  }
+
+  addEncounter(mrn, encounter) {
+    let newEncounters = {...this.state.patients}
+    newEncounters['patient_' + mrn].encounters['encounter_' + encounter.visitNumber] = encounter
+    this.setState({
+      patients: newEncounters
     })
   }
 
@@ -73,12 +82,31 @@ class App extends Component {
       this.removePatient(mrn)
     }
     let newPatients = {...this.state.patients}
-    newPatients['patient_' + mrn] = newPatient
+    newPatients['patient_' + newPatient.mrn] = newPatient
     this.setState({
       patients: newPatients
     })
   }
 
+  editEncounter(mrn, visitNumber, encounter) {
+    if (visitNumber !== encounter.visitNumber) {
+      this.removeEncounter(mrn, 'encounter_' + visitNumber)
+    }
+    let newPatientsEncounters = {...this.state.patients}
+    newPatientsEncounters['patient_' + mrn].encounters['encounter_' + encounter.visitNumber] = encounter
+    this.setState({
+      patients: newPatientsEncounters
+    })
+  }
+
+  getPatient(mrn) {
+    return this.state.patients['patient_' + mrn]
+  }
+
+  getEncounter(mrn, visitNumber) {
+    return this.state.patients['patient_' + mrn].encounters['encounter_' + visitNumber]
+  }
+  //<EncounterDetailsView {...props} encounter={this.getEncounter(...props.match.params)} editEncounter={this.editEncounter}/>
   render() {
     return (
       <Router>
@@ -86,12 +114,26 @@ class App extends Component {
           <Route exact path="/" render={(props) => 
             <PatientOverview {...props} data={this.state} />
           }/>
-          <Route exact path="/edit/:mrn" render={(props) =>
+          <Route exact path="/patient/:mrn/edit" render={(props) =>
             <EditPatientView {...props} editPatient={this.editPatient} />
           }/>
-          <Route exact path="/patients/:mrn" component={PatientDetailsView} />
+          <Route exact path="/patient/:mrn" render={(props) =>
+            <PatientDetailsView {...props} patient={this.getPatient(props.match.params.mrn)} header={this.state.encountersHeader} remove={this.state.removeEncounter} />
+          }/>
+          <Route exact path="/patient/:mrn/encounter/add" render={(props) =>
+            <CreateEncounterView {...props} addEncounter={this.addEncounter} />
+          }/>
+          <Route exact path="/patient/:mrn/encounter/:visitNumber" render={(props) =>
+            <EncounterDetailsView {...props} encounter={this.getEncounter(props.match.params.mrn, props.match.params.visitNumber)} />
+          }/>
+          <Route exact path="/patient/:mrn/encounter/:visitNumber/edit" render={(props) => 
+            <EditEncounterView {...props} encounter={this.getEncounter(props.match.params.mrn, props.match.params.visitNumber)} editEncounter={this.editEncounter} />
+          }/>
           <Route exact path="/add/patient" render={(props) =>
             <CreatePatientView {...props} addPatient={this.addPatient} />
+          }/>
+          <Route exact path="/patient/:mrn/encounter/add" render={(props) =>
+            <CreateEncounterView {...props} addEncounter={this.addEncounter} />
           }/>
         </Switch>
       </Router>
